52ced757fcec4baa0d734d18ee0de944b387450f
Annoyer <247221925@qq.com>
2017-11-03 15:05:40 +0800

    branch test by jcy

---
+++ src/main/java/com/xq/controller/OrganizationController.java
@@ -0,0 +1,13 @@
+package com.xq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 机构查询和显示
+ * Created by joy12 on 2017/11/3.
+ */
+@Controller
+@RequestMapping("")
+public class OrganizationController {
+}
